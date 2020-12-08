6ec6651547b5bfa0202bd5316bfe8d3ae397197b
Ronaldo Gaston <ronaldogaston@outlook.com>
2020-12-08 12:02:56 -0300

    Camada de serviço, registro de componente

---
+++ src/main/java/com/ronaldogaston/course/services/UserService.java
@@ -0,0 +1,26 @@
+package com.ronaldogaston.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ronaldogaston.course.entities.User;
+import com.ronaldogaston.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
