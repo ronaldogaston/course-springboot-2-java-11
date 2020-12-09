6d6aae46cb207252a7bfbaf557f0c00dc1948f68
Ronaldo Gaston <ronaldogaston@outlook.com>
2020-12-08 23:26:45 -0300

    Associação um para muito, Produto - Itens do Pedido

--- src/main/java/com/ronaldogaston/course/entities/Product.java
+++ src/main/java/com/ronaldogaston/course/entities/Product.java
@@ -11,7 +11,10 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "tb_product")
@@ -33,6 +36,9 @@ public class Product implements Serializable{
 	inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>(); // Para o produto não poder ter mais de uma categoria
 
+	@OneToMany(mappedBy = "id.product")
+	private Set<OrderItem> items = new HashSet<>(); // Set para não admitir repetições
+	
 	public Product() {
 		super();
 	}
@@ -88,6 +94,15 @@ public class Product implements Serializable{
 
 	public Set<Category> getCategories() {
 		return categories;
+	}
+	
+	@JsonIgnore
+	public Set<Order> getOrders() {
+		Set<Order> set = new HashSet<>();
+		for (OrderItem x : items) {
+			set.add(x.getOrder());
+		}
+		return set;
 	}
 
 	@Override
