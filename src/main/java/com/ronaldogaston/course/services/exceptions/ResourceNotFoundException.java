4afe662a1696d8c78f2dffa50dbd2582dfff6b1f
Ronaldo Gaston <ronaldogaston@outlook.com>
2020-12-11 15:09:40 -0300

    Criando Exceção para Pesquisa de Usuário por ID

---
+++ src/main/java/com/ronaldogaston/course/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package com.ronaldogaston.course.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundException(Object id) {
+		super("Resource not found. Id " + id);
+	}
+
+}
